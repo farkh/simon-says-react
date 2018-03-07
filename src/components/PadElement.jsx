78c690f99de419d19bcd4090431f27a2d5f5012c
Ainur Farkh <farkh@MacBook-Farkh.local>
2018-03-07 22:27:25 +0300

    Ooops

--- src/components/PadElement.jsx
+++ src/components/PadElement.jsx
@@ -3,15 +3,10 @@ import React, { Component } from 'react';
 class PadElement extends Component {
 
 	render() {
-		let elementStyles = {
-			background: this.props.color
-		};
-
 		return (
 			<div 
 				className="pad__element"
 				data-index={this.props.index}
-				style={elementStyles}
 			></div>
 		);
 	}
